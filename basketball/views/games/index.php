14124f303f2b2d2874e35703748e9779ebd90d89
Hiroshi irimoto <toyota40906@gmail.com>
2016-01-30 09:32:04 +0800

    試合編集・作成・詳細ページに移動できるように<a>タグを追加

--- basketball/views/games/index.php
+++ basketball/views/games/index.php
@@ -1,6 +1,5 @@
 <?php
 echo '試合一覧';
-
 $all_games = $this->view_options['all_games'];
 //var_dump($all_games);
 foreach ($all_games as $row) {
@@ -26,7 +25,9 @@ foreach ($all_games as $row) {
   echo $row['modified'].'<br />';
   echo $row['cancelled'].'<br />';
 ?>
+<a href="create">試合作成</a>
 <a href="show/<?php echo $row['id']; ?>">試合詳細</a>
+<a href="edit/<?php echo $row['id'];?>">試合編集</a>
 <br />
 <br />
 
