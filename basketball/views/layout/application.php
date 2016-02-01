7e433ec017cc6f7edf0d885169978524b4f4e296
ta9yeah <matsui.takuya.2sb@gamil.com>
2016-02-01 11:43:44 +0800

    application 修正

--- basketball/views/layout/application.php
+++ basketball/views/layout/application.php
@@ -1,24 +1,3 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>バスケ</title>
-<script type="text/javascript">
-function game_delete(){
-  
-  if (window.confirm("Are you sure?")) {
-    return true;
-  }else{
-    return false;
-  }
-}
-</script>
-</head>
-
-<body>
-    <?php
-      include('./views/' . $this->resource . '/' . $this->action . '.php');
-    ?>
-
-</body>
-</html>
+<?php
+  include('./views/' . $this->resource . '/' . $this->action . '.php');
+?>
