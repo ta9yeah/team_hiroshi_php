596247a8ec653d53050cfc8dcb30152262df09f6
Hiroshi irimoto <toyota40906@gmail.com>
2016-02-09 10:26:06 +0800

    マイページ

--- basketball/models/my_page.php
+++ basketball/models/my_page.php
@@ -54,6 +54,25 @@ class My_page{
     return $return;
   }
 
+  //アカウント作成
+  public function signup($box6){
+    //INSERTのSQL文
+    $sql = 'INSERT INTO `users` SET   username = "'.$box6['email'].'", 
+                                      password="'.$box6['password'].'",
+                                      created=NOW();';
+    mysqli_query($this->dbconnect,$sql) or die(mysqli_error($this->dbconnect));
+
+    $name = $box6['name']; 
+    $user_id = mysqli_insert_id($this->dbconnect);
+    // var_dump($user_id);
+    $sql = 'INSERT INTO `user_profiles` SET user_id='.$user_id.',name="'.$name.'",created=NOW();';
+    mysqli_query($this->dbconnect,$sql) or die(mysqli_error($this->dbconnect));
+
+    // $user_id2 = $user_id;
+    // return $user_id2 =$user_id;
+    // var_dump($user_id2);
+
+  }
 }
 
 
